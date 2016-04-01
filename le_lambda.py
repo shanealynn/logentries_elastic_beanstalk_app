18842940fbb3cfde5628285e054dd15db8f42075
shanealynn <shane.a.lynn@gmail.com>
2016-04-01 18:16:26 +0100

    Uploading edited le_lambda.py file with zlib

---
+++ le_lambda.py
@@ -0,0 +1,117 @@
+import boto3
+import socket
+import ssl
+import datetime
+import re
+import csv
+import zlib
+import StringIO
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+# This token is used to associate log files in AWS S3 to a log in your Logentries account.
+log_token = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+# You can supply an optional token to log activity to a log on Logentries and any errors from this script.
+# This is optional, it is recommended you use one log file/token for all your Lambda scripts. If you do not
+# wish to use this, just leave the value blank.
+debug_token = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+# Log to generic activity from this script to our support logging system for Lambda scripts
+# this is optional, but helps us improve our service nad can be hand for us helping you debug any issues
+# just remove this token if you wish (leave variable in place)
+lambda_token = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+username = "info@killbiller.com"
+
+
+def lambda_handler(event, context):
+    host = 'data.logentries.com'
+    port = 20000
+    s_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = ssl.wrap_socket(s_, ca_certs='le_certs.pem', cert_reqs=ssl.CERT_REQUIRED)
+    s.connect((host, port))
+    tokens = []
+    if validate_uuid(debug_token) is True:
+        tokens.append(debug_token)
+    if validate_uuid(lambda_token) is True:
+        tokens.append(lambda_token)
+    else:
+        pass
+
+    if validate_uuid(log_token) is False:
+        for token in tokens:
+            s.sendall('%s %s\n' % (token, "{}: log token not present for username={}"
+                                   .format(str(datetime.datetime.utcnow()), username)))
+        raise SystemExit
+    else:
+        # Get the object from the event and show its content type
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = event['Records'][0]['s3']['object']['key']
+        try:
+            # Get object from S3 Bucket
+            response = s3.get_object(Bucket=bucket, Key=key)
+            body = response['Body']
+            # Read data from object, at this point this is compressed zip binary format.
+            compressed_data = body.read()
+            s.sendall("Successfully read the zipped contents of the file.")
+            # Use zlib library to decompress this binary string
+            data = zlib.decompress(compressed_data, 16+zlib.MAX_WBITS)
+            s.sendall("Decompressed data")
+            # Now continue script as normal to send data to Logentries.
+            for token in tokens:
+                s.sendall('%s %s\n' % (token, "username='{}' downloaded file='{}' from bucket='{}' and uncompressed!."
+                                       .format(username, key, bucket)))
+            lines = data.split("\n")
+            for token in tokens:
+                s.sendall('%s %s\n' % (token, "Beginning to send lines='{}' start_time='{}'."
+                                       .format(str(len(lines)), str(datetime.datetime.utcnow()))))
+            if validate_elb_log(str(key)) is True:
+                # timestamp elb client:port backend:port request_processing_time backend_processing_time
+                # response_processing_time elb_status_code backend_status_code received_bytes sent_bytes
+                # "request" "user_agent" ssl_cipher ssl_protocol
+                rows = csv.reader(data.splitlines(), delimiter=' ', quotechar='"')
+                for line in rows:
+                    request = line[11].split(' ')
+                    idx = request[1].find('/', 9)
+                    url = request[1][idx:]
+                    parsed = {
+                        'ip': line[2].split(':')[0],
+                        'method': request[0],
+                        'url': url,
+                        'user_agent': line[12]
+                    }
+                    msg = "\"{0}\" ip=\"{ip}\" request_time=\"{5}\" elb_status=\"{7}\" backend_status=\"{8}\"" \
+                          " bytes_received=\"{9}\" bytes_sent=\"{10}\" method=\"{method}\" url=\"{url}\"" \
+                          " user_agent=\"{user_agent}\"\n"\
+                        .format(*line, **parsed)
+                    s.sendall(log_token + msg)
+            else:
+                for line in lines:
+                    s.sendall('%s %s\n' % (log_token, line))
+            for token in tokens:
+                s.sendall('%s %s\n' % (token, "username='{}' finished sending log data end_time='{}'"
+                                       .format(username, str(datetime.datetime.utcnow()))))
+        except Exception as e:
+            for token in tokens:
+                print e
+                s.sendall(e.strerror)
+                s.sendall('%s %s\n' % (token, "Error getting username='{}' file='{}' from bucket='{}'. Make sure "
+                                              "they exist and your bucket is in the same region as this function."
+                                       .format(username, key, bucket)))
+        finally:
+            s.close()
+
+
+def validate_uuid(uuid_string):
+    regex = re.compile('^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$', re.I)
+    match = regex.match(uuid_string)
+    return bool(match)
+
+
+def validate_elb_log(key):
+    regex = re.compile('^\d+_\w+_\w{2}-\w{4,9}-[12]_.*._\d{8}T\d{4}Z_\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}_.*.log$', re.I)
+    match = regex.match(key)
+    return bool(match)
